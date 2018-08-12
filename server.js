fab443231a25d79aa086a948a1ba62f42079cb7d
SIDDHARTH DEORE <siddharthdeore@gmail.com>
2018-08-12 04:27:25 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express();
-var server = app.listen(3000);
+var server = app.listen(process.env.PORT);
 
 app.use(express.static('public')); // location of public directory
 
