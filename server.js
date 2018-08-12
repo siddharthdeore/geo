55cb6e11f8702e1a8f412762f51d945d5f0d3d54
SIDDHARTH DEORE <siddharthdeore@gmail.com>
2018-08-12 04:24:43 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+
+var app = express();
+var server = app.listen(3000);
+
+app.use(express.static('public')); // location of public directory
+
+console.log("Server Started");
+
+var socket = require('socket.io');
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+var users = {};
+
+function newConnection(socket) {
+	console.log('connected: ' + socket.id);
+	socket.emit('UpdateOthers',users);
+
+	socket.on('newLocation', function(user){
+		users[socket.id]= {
+			lat:user.lat,
+			lon:user.lon,
+			id: socket.id
+		}
+		socket.broadcast.emit('newUser',users[socket.id]);		
+		socket.emit('UpdateOthers',users);
+	});
+	socket.on('disconnect', function () {
+		  console.log('disconnected: ' + socket.id);
+		 delete users[socket.id];
+		// emit a message to all users to remove this player
+		socket.broadcast.emit('userLeft',users);
+		io.emit('disconnect', socket.id);
+	});
+
+}
